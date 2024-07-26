0b831feb904a7513d768bdfdc37147dc88d83c12
iamggragas <iamggragas@gmail.com>
2024-07-26 18:36:40 -0400

    Home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: 'George'}
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type="greeting" 
+            title="Hello" 
+            user={loggedIn?.firstName || 'guest'} 
+            subtext="Access and manage your accounts and transactions effieciently"
+          />  
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.90}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
